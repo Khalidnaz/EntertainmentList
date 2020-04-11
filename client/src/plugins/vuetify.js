0bf49c2baf48f22ff248ad7d76ad7d8440d78628
Gilbert Wong <gilwong3105@gmail.com>
2020-04-10 19:12:27 -0700

    Add vuetify

---
+++ client/src/plugins/vuetify.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib';
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  iconfont: 'md',
+  theme: {
+    primary: '#3B125F',
+    secondary: '#8B5FBF',
+    accent: '#BF653F',
+    error: '#722530',
+    warning: '#A37513',
+    info: '#396893',
+    success: '#4CAF50'
+  }
+});
